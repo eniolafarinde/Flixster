cfbb66097b1e66099612761d50a59e7d5e560502
Eniola Olawumi Farinde <eniolafarinde@gmail.com>
2025-06-11 18:58:39 -0700

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
-      <MovieList/>    
+      <MovieList/>
+      <Footer/>
     </div>
   )
 }
