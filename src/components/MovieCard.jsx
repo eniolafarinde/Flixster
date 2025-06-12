3b917227aa79ce0cf9c20d8aeb857a7a86c33e7b
Eniola Olawumi Farinde <eniolafarinde@gmail.com>
2025-06-11 19:57:06 -0700

    added watched button

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import './MovieCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as farHeart, faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark as farBookmark } from '@fortawesome/free-regular-svg-icons';
+import { faBookmark as fasBookmark } from '@fortawesome/free-solid-svg-icons';
 
 const MovieCard = ({ movie, onClick }) => {
   const [isLiked, setIsLiked] = useState(false);
-
+  const [isWatched, setIsWatched] = useState(false);
   const imageUrl = movie.poster_path
     ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
     : 'https://via.placeholder.com/185x278?text=No+Image';
@@ -13,10 +17,13 @@ const MovieCard = ({ movie, onClick }) => {
       onClick(movie.id);
     }
   };
-
   const handleLikeButtonClick = (e) => {
     e.stopPropagation();
     setIsLiked(!isLiked);
+  };
+  const handleWatchedButtonClick = (e) => {
+    e.stopPropagation();
+    setIsWatched(!isWatched);
   };
 
   return (
@@ -30,11 +37,12 @@ const MovieCard = ({ movie, onClick }) => {
       <div className="movie-info">
         <h3>{movie.title}</h3>
         <p>Rating: {movie.vote_average.toFixed(1)}</p>
-        <div className="like-button-container">
-          <button className="like-button"  onClick={handleLikeButtonClick} aria-label={isLiked ? 'Unlike' : 'Like'}>
-            <span className={`heart-icon ${isLiked ? 'liked' : ''}`}>
-              {isLiked ? '♥' : '♡'}
-            </span>
+        <div className="action-buttons">
+          <button className={`icon-button ${isLiked ? 'active' : ''}`} onClick={handleLikeButtonClick} aria-label={isLiked ? 'Unlike' : 'Like'} >
+            <FontAwesomeIcon icon={isLiked ? fasHeart : farHeart} />
+          </button>
+          <button className={`icon-button ${isWatched ? 'active' : ''}`} onClick={handleWatchedButtonClick} aria-label={isWatched ? 'Unmark as watched' : 'Mark as watched'} >
+            <FontAwesomeIcon icon={isWatched ? fasBookmark : farBookmark} />
           </button>
         </div>
       </div>
