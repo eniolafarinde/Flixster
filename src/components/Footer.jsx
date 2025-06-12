cbe3406eed34aa5d24b8423ffdfc314a29d7a3b0
Eniola Olawumi Farinde <eniolafarinde@gmail.com>
2025-06-11 18:57:39 -0700

    added header and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// src/Footer.jsx
+import React from 'react';
+import './Footer.css'; // Don't forget to create this CSS file!
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>&copy; 2025 Flixster. Eniola Olawumi Farinde.</p>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import './Header.css'; // Don't forget to import the CSS file!
 
 const Header = () => {
     return (
         <header className="header">
-            <h1>Flixster</h1>
+            <div className="header-content">
+                <h1 className="logo">🎥 Flixster</h1>
+                <p className="welcome-message">Welcome to an unlimited world of movies!</p>
+            </div>
         </header>
     );
 }
 
-export default Header
+export default Header;
