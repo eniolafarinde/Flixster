97308d7d8e8d4d32de4a573a0f47abafeb295d36
Eniola Olawumi Farinde <eniolafarinde@gmail.com>
2025-06-13 12:14:37 -0700

    edited the files to have indented

--- src/components/sortControls.jsx
+++ src/components/sortControls.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const SortControls = ({ sortOrder, onChange }) => (
-<div className="sort-controls">
-<label htmlFor="sort-by">Sort by:</label>
-<select id="sort-by" value={sortOrder} onChange={onChange}>
-<option value="popularity.desc">Most Popular</option>
-<option value="original_title.asc">Title (A-Z)</option>
-<option value="release_date.desc">Most Recent</option>
-<option value="vote_average.desc">Highest Rating</option>
-</select>
-</div>
+    <div className="sort-controls">
+        <label htmlFor="sort-by">Sort by:</label>
+        <select id="sort-by" value={sortOrder} onChange={onChange}>
+        <option value="popularity.desc">Most Popular</option>
+        <option value="original_title.asc">Title (A-Z)</option>
+        <option value="release_date.desc">Most Recent</option>
+        <option value="vote_average.desc">Highest Rating</option>
+        </select>
+    </div>
 );
 
 export default SortControls;
--- src/components/viewToggleButtons.jsx
+++ src/components/viewToggleButtons.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 
 const ViewToggleButtons = ({ activeView, setActiveView, setPage, setMovies }) => {
-const handleNowPlayingClick = () => {
-setActiveView('nowPlaying');
-setPage(1);
-setMovies([]);
-};
+    const handleNowPlayingClick = () => {
+        setActiveView('nowPlaying');
+        setPage(1);
+        setMovies([]);
+    };
+    const handleSearchViewClick = () => {
+        setActiveView('searchResults');
+        setPage(1);
+        setMovies([]);
+    };
 
-const handleSearchViewClick = () => {
-setActiveView('searchResults');
-setPage(1);
-setMovies([]);
-};
-
-return (
-<div className="view-toggle-buttons">
-<button className={activeView === 'nowPlaying' ? 'active' : ''} onClick={handleNowPlayingClick}>
-Now Playing
-</button>
-<button className={activeView === 'searchResults' ? 'active' : ''} onClick={handleSearchViewClick}>
-Search Results
-</button>
-</div>
-);
+    return (
+        <div className="view-toggle-buttons">
+            <button className={activeView === 'nowPlaying' ? 'active' : ''} onClick={handleNowPlayingClick}> Now Playing </button>
+            <button className={activeView === 'searchResults' ? 'active' : ''} onClick={handleSearchViewClick}> Search Results</button>
+        </div>
+    );
 };
 
 export default ViewToggleButtons;
